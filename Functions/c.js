b35faf3f1a228be2e9cf9790e324e368e61f6760
romeshjainn <romeshjainofficial@gmail.com>
2023-08-10 09:49:23 +0530

    h

--- Functions/c.js
+++ Functions/c.js
@@ -1,8 +1,8 @@
 const addNumV1 = (a, b) => {
   return a + b;
 };
-
-const addNumV2 = (a, b) => a + b;
+console.log(addNumV2)
+var addNumV2 = (a, b) => a + b;
 
 console.log(addNumV1(10, 20));
 console.log(addNumV2(100, 20));
