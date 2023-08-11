268d41323df0d74be3492a210483bc8861c2d44c
romeshjainn <romeshjainofficial@gmail.com>
2023-08-11 08:51:50 +0530

    arrow function

---
+++ Functions/arrowFunction.js
@@ -0,0 +1,26 @@
+/**
+ * It is known as the following
+ * - Function expressions
+ * - Variable functions
+ */
+
+let add = (a, b) => a + b;
+console.log(add(1,2));
+
+
+// const addV1 = function (a, b) {
+//   return a + b;
+// };
+// const add = (a, b) => a + b;
+// const multiply = (a, b) => a * b;
+
+// const customFunction = (a, b) => {
+//   const sum = a + b;
+//   const mul = a * b;
+//   return sum + mul;
+// };
+
+// console.log("addV1: ", addV1(10, 80));
+// console.log("add: ", add(100, 80));
+// console.log("multiply: ", multiply(10, 80));
+// console.log("customFunction: ", customFunction(10, 80));
