d98b97d707f4df32c287c8a337548b4064e7aefb
romeshjainn <romeshjainofficial@gmail.com>
2023-08-15 20:32:18 +0530

    spread op

---
+++ objSpread.js
@@ -0,0 +1,21 @@
+// let profile = { name: "sanjeev", no: 1 };
+// let profile2 = profile;
+// profile2.name = "romesh";
+// console.log("profile", profile);
+// console.log("profile2", profile2);
+
+
+// let profile = { name: "sanjeev", no: 1 };
+
+// let profile2 = { ...profile, class: "VSS", name:"nitin chugh"}
+// console.log("profile", profile);
+// console.log("profile2", profile2);
+
+
+// let a = { name: "romesh", b: 1, nestedObject: { profile: "hello", num: 1, }, };
+
+// let b = { ...a };
+// b.nestedObject.profile = "sanjeev";
+
+// console.log("a",a)
+// console.log("b",b)
