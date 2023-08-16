1b23520e8abcf052fd07c5ebaa66087ec1ed2aca
romeshjainn <romeshjainofficial@gmail.com>
2023-08-16 09:52:32 +0530

    na

---
+++ Map, Filter, Reduce/map2.js
@@ -0,0 +1,12 @@
+// const nums = [1, 2, 3, 4, 5, 6, 7];
+
+// const multipleByTwo = nums.map((currentItem, index, arr) => {
+//     console.log({ currentItem, index, arr });
+// //   return currentItem * 2;
+// });
+
+// const multipleByTwoV2 = nums.map((currentItem) => currentItem * 2);
+
+// console.log({ nums, multipleByTwo, multipleByTwoV2 });
+
+
---
+++ Map, Filter, Reduce/que.js
@@ -0,0 +1,39 @@
+const arr = [
+  {
+    id: 1,
+    name: "John Doe",
+    city: "New York",
+    state: "New York",
+    isActive: true,
+  },
+  {
+    id: 2,
+    name: "Jane Smith",
+    city: "Los Angeles",
+    state: "California",
+    isActive: false,
+  },
+  {
+    id: 3,
+    name: "Michael Johnson",
+    city: "Chicago",
+    state: "Illinois",
+    isActive: true,
+  },
+  {
+    id: 4,
+    name: "Emily Brown",
+    city: "Houston",
+    state: "Texas",
+    isActive: true,
+  }
+];
+let newArr = arr.map(function (element) {
+    if(element.isActive == false) {
+        element.isActive = "na";
+    }
+    return element;
+});
+
+console.log(arr)
+console.log(newArr)
