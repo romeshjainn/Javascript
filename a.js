e8e7cb0ca6853aa00b81a44ec3a11b7c8c47afac
Romesh Jain <romeshjainofficial@gmail.com>
2023-08-21 20:14:20 +0530

    update

--- a.js
+++ a.js
@@ -3,22 +3,17 @@
 
 function check(a) {
   let b = a.split("");
-    if (b[0] === b[1] || b[2] === b[3] || b[4] === b[5]) {
-        return true;
-    }
-    else {
-        return false;
-    }
-  //   if (
-  //     (b[0] === "(" && b[1] === ")") ||
-  //     (b[0] === "[" && b[1] === "]") ||
-  //     (b[0] === "{" && b[1] === "}")
-  //   ) {
-  //     return true;
-  //   } else {
-  //     return false;
-  //   }
+
+  if (
+    (b[0] === "(" && b[1] === ")") ||
+    (b[0] === "[" && b[1] === "]") ||
+    (b[0] === "{" && b[1] === "}")
+  ) {
+    return true;
+  } else {
+    return false;
+  }
 }
 
-let a = "(){}";
+let a = "(){}[}";
 console.log(check(a));
