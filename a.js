ce80edf1da0436be77474f91decc129d4a489d16
Romesh Jain <romeshjainofficial@gmail.com>
2023-08-21 20:00:41 +0530

    update

--- a.js
+++ a.js
@@ -1,6 +1,11 @@
+// taking string and converting it to array
+// using if else, to check if the brackets have there partner closing bracket
+
 function check(a) {
   let b = a.split("");
-
+  // for (let i = 0; i < b.length; i++){
+  // trying another way
+  // }
   if (
     (b[0] === "(" && b[1] === ")") ||
     (b[0] === "[" && b[1] === "]") ||
@@ -12,5 +17,5 @@ function check(a) {
   }
 }
 
-let a = "()";
+let a = "(){}";
 console.log(check(a));
