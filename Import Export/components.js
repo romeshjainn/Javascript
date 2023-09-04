c8eaeaa906ae5cd71556b32e91c93edfb4418458
Romesh Jain <romeshjainofficial@gmail.com>
2023-09-04 09:13:26 +0530

    import and export

---
+++ Import Export/components.js
@@ -0,0 +1,6 @@
+export const greetings = () => {
+    return "romesh";
+}
+
+export let country = "india";
+export let phone = "samsung";
---
+++ Import Export/script.js
@@ -0,0 +1,5 @@
+import { greetings, country, phone as phoneNumber } from "./components.js";
+
+console.log(greetings());
+console.log(country);
+console.log(phoneNumber);
