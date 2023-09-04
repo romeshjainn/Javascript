c0887f413813cf070af62c022b6c9b28c1b5103f
Romesh Jain <romeshjainofficial@gmail.com>
2023-09-04 09:34:29 +0530

    update

--- Import Export/components.js
+++ Import Export/components.js
@@ -1,6 +1,14 @@
-export const greetings = () => {
-    return "romesh";
-}
+// export const greetings = () => {
+//     return "romesh";
+// }
 
-export let country = "india";
-export let phone = "samsung";
+// export let country = "india";
+// export let phone = "samsung";
+const greetings = () => {
+  return "romesh";
+};
+
+let country = "india";
+let phone = "samsung";
+
+export { greetings, country, phone };
